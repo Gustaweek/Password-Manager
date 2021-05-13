47d26af868c0a5441f928d04f5eb81434aba4ed9
JakubMichalMalinowski <jakumal648@student.polsl.pl>
2021-05-13 22:18:18 +0200

    init index

---
+++ src/main/java/com/pkiks1/passwordmanager/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package com.pkiks1.passwordmanager.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping({"", "/", "index", "index.html"})
+    public String index(Model model) {
+        return "index";
+    }
+}

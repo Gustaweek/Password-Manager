e24ba3be8bc1f687f157b4fbc9a616c17b50fa6a
Mateusz Kus <48448513+Gustaweek@users.noreply.github.com>
2021-05-16 21:02:46 +0200

    add method createCredential

--- src/main/java/com/pkiks1/passwordmanager/services/CredentialService.java
+++ src/main/java/com/pkiks1/passwordmanager/services/CredentialService.java
@@ -1,6 +1,7 @@
 package com.pkiks1.passwordmanager.services;
 
 
+import com.pkiks1.passwordmanager.domain.CredentialEntity;
 import com.pkiks1.passwordmanager.dto.CredentialDto;
 import com.pkiks1.passwordmanager.repositories.CredentialRepository;
 import com.pkiks1.passwordmanager.repositories.UserRepository;
@@ -11,21 +12,21 @@ import org.springframework.stereotype.Service;
 public class CredentialService {
 
     private final CredentialRepository credentialRepository;
-    // private final UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    public CredentialService(CredentialRepository credentialRepository) {
-        this.credentialRepository = credentialRepository;
-    }
-/*
     public CredentialService(CredentialRepository credentialRepository, UserRepository userRepository) {
         this.credentialRepository = credentialRepository;
         this.userRepository = userRepository;
-    }*/
+    }
 
     public void createCredential (CredentialDto credentialDto){
+        //TODO validation credentialDto
 
-
+        credentialRepository.save(new CredentialEntity(credentialDto.getTitle(),
+                credentialDto.getEmail(),
+                credentialDto.getPassword(),
+                userRepository.findById(credentialDto.getUserId()).get()));
     }
 
 }

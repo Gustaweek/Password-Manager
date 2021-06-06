61344f83729d01d86f959f4bceca5f28262d7a50
JakubMichalMalinowski <jakumal648@student.polsl.pl>
2021-06-07 00:50:31 +0200

    init index

--- src/main/java/com/pkiks1/passwordmanager/security/SecurityConfig.java
+++ src/main/java/com/pkiks1/passwordmanager/security/SecurityConfig.java
@@ -27,6 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin().loginPage("/login").permitAll().defaultSuccessUrl("/dashboard", true);
+        http.authorizeRequests().antMatchers("/").permitAll();
         http.authorizeRequests().antMatchers("/register").permitAll();
         http.authorizeRequests().antMatchers("/password-generator").permitAll();
         http.authorizeRequests().antMatchers("/images/**", "/scripts/**", "/styles/**").permitAll();
